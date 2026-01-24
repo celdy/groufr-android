9f84196ad72ca129d500e039a3f97be33d455ebc
celdy <info@celdy.com>
2026-01-24 10:54:55 +0100

    Version 0.03 When showing date and it is in different days then it is displayed on multiple lines (start - end)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.celdy.groufr"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.02"
+        versionCode = 3
+        versionName = "0.03"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
