d7444d6f151e47f7a06ea6aa70bf52eb7dce56c6
Kristina Kopecka <info@celdy.com>
2026-02-06 17:07:24 +0100

    Version 16 - 0.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.celdy.groufr"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.15"
+        versionCode = 16
+        versionName = "0.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
